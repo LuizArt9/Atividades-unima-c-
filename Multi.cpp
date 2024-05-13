5ca1783f978b10906e999a165445838696dffa47
LuizArt9 <166402385+LuizArt9@users.noreply.github.com>
2024-05-13 19:14:55 -0300

    Create Multi.cpp

---
+++ Multi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, multiplicacao;
+    cout << "Digite o primeiro número: ";
+    cin >> num1;
+    cout << "Digite o segundo número: ";
+    cin >> num2;
+    multiplicacao = num1 * num2;
+    cout << "A multiplicação é: " << multiplicacao << endl;
+    return 0;
+}
