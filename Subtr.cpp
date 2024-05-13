4d3f952fdceb318c68ac3bf4857c01e7c28d1c34
LuizArt9 <166402385+LuizArt9@users.noreply.github.com>
2024-05-13 19:12:16 -0300

    Create Subtr.cpp

---
+++ Subtr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, subtracao;
+    cout << "Digite o primeiro número: ";
+    cin >> num1;
+    cout << "Digite o segundo número: ";
+    cin >> num2;
+    subtracao = num1 - num2;
+    cout << "A subtração é: " << subtracao << endl;
+    return 0;
+}
